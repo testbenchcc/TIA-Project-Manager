49c4570927f804b954dff120e19d5b37bb083fe3
testbenchcc <vps@testbench.cc>
2025-06-29 10:44:14 -0600

    init; interface work;

---
+++ Interface/BoolToGlyphConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Interface      // same namespace as your other code
+{
+    public class BoolToGlyphConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool expanded = value as bool? ?? false;
+            return expanded ? "▼" : "▲";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => throw new NotSupportedException();
+    }
+}
